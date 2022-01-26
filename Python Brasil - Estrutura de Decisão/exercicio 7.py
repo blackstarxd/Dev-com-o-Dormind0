857ac5aff9bd5ca9275eeeed8ab5ff305c764db5
Your Name <you@example.com>
2022-01-25 21:58:13 -0300

    feito até o 7 pq o 6 é parecido.

--- Python Brasil - Estrutura de Decisão/exercicio 7.py
+++ Python Brasil - Estrutura de Decisão/exercicio 7.py
@@ -12,8 +12,9 @@ segundo_numero = int(input("Digite o segundo numero: "))
 terceiro_numero = int(input("Digite o terceiro numero: "))
 numeros = [primeiro_numero, segundo_numero, terceiro_numero]
 list.sort(numeros)
+menor = numeros[0]
 maior = numeros[2]
-print(f'O maior número: {maior}.')
+print(f'O maior número: {maior}. O menor número: {menor}.')
 
 if(primeiro_numero > segundo_numero or primeiro_numero > terceiro_numero):
     print("O primeiro numero escolhido foi o maior: " +str(primeiro_numero))
@@ -23,10 +24,11 @@ else:
     else:
         print("O segundo numero escolhido foi o maior: " +str(segundo_numero))
 
-# maior_numero = None
 
-# for num in numeros:
-#     if (maior_numero is None or num > maior_numero):
-#         maior_numero = num
-
-# print('O maior número foi:', maior_numero)
+if(primeiro_numero < segundo_numero or primeiro_numero < terceiro_numero):
+    print("O primeiro numero escolhido foi o menor: " +str(primeiro_numero))
+else:
+    if(segundo_numero > primeiro_numero or segundo_numero > terceiro_numero):
+        print("O terceiro numero escolhido foi o menor: " +str(terceiro_numero))
+    else:
+        print("O segundo numero escolhido foi o menor: " +str(segundo_numero))
