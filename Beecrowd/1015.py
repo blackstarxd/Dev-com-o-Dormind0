6cc3097c6b0e04dde51ded09d879140104cf2a9a
Your Name <you@example.com>
2022-03-02 22:08:46 -0300

    A solucao nao deve conter textos dentro de Input ou Print, se nao for pedido.

--- Beecrowd/1015.py
+++ Beecrowd/1015.py
@@ -7,9 +7,10 @@
 
 # print(f"A distância entre os dois pontos é: {distancia:.4f}")
 
+#A solução no Beecrowd leva em conta até mesmo se for digitado algum texto a mais dentro do Input ou Print.
 
-p1  = list(map(float, input("Insira os dois valores de ponto flutuante: x1 e y1: ").split()))
-p2  = list(map(float, input("Insira os dois valores de ponto flutuante: x2 e y2: ").split()))
+p1  = list(map(float, input().split()))
+p2  = list(map(float, input().split()))
 
 x1 = p1[0]
 y1 = p1[1]
@@ -19,4 +20,4 @@ y2 = p2[1]
 
 distancia = ((x2 - x1)**2 + (y2 - y1)**2) ** 0.5
 
-print(f"A distância entre os dois pontos é: {distancia:.4f}")
+print(f"{distancia:.4f}")
